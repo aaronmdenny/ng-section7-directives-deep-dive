3558ef35a305d983edc5f4965492d0037b33de30
aaronmdenny <aaronmdenny@outlook.com>
2021-07-04 22:01:47 -0400

    using HostListener to listen to host events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -29,7 +29,29 @@ export class BetterHighlightDirective implements OnInit {
      * 
      * Learn more about the renderer here: https://angular.io/api/core/Renderer2
      */
+    /**
+     * We can make this directive more interactive. Perhaps we want to only make the background color blue if the user
+     * hovers over the element with the mouse.
+     * 
+     * We need to react to some events occurring on the element the directive sits on. We can accomplish this with the
+     * @HostListener() decorator.
+     */
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  /**
+   * We attach @HostListener() to a method we want to execute (mouseOver()). We specify the event we want to use to
+   * trigger the mouseOver() method in the argument to HostListener().
+   * 
+   * The mouseenter event is supported by the element this directive sits on (a p element).
+   * 
+   * Event data is passed via the parameter. We can also listen to custom events here.
+   */
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
 }
